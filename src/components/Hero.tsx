cc55c1ebb00201e44481e9af063dfdb67d254457
Rahul Pahuja <rahulpahuja2015@gmail.com>
2025-06-15 16:06:11 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { ArrowRight, Mail, FileText, Github, Linkedin } from 'lucide-react';
-
+import resume from '../../resume.pdf'; // adjust path as needed
 const Hero = () => {
   return (
     <section id="home" className="pt-32 pb-20 md:pt-40 md:pb-24 text-center">
@@ -37,7 +37,7 @@ const Hero = () => {
             icon={<Mail className="h-5 w-5" />}
           />
           <ActionButton 
-            href="resume.pdf"
+            href="{resume}"
             primary={false}
             text="Download Resume"
             icon={<FileText className="h-5 w-5" />}
